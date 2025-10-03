36eb7483b954aee0898c560cca3a357391a692c6
vasylovyevhenclg-tech <vasylov.yevhen.clg@chnu.edu.ua>
2025-10-04 02:36:35 +0300

    Update math_lib.cpp
    123123

--- src/math_lib/math_lib.cpp
+++ src/math_lib/math_lib.cpp
@@ -44,4 +44,11 @@ namespace MathLib
 	}
 }
 
-int factorial(int n);
+int factorial(int n) {
+         if (n < 0) return -1; 
+         int result = 1;
+         for (int i = 1; i <= n; i++) {
+             result *= i;
+         }
+         return result;
+     }

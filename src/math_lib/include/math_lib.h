731bd944c1d9f14e5fa3488e3d0eedb1cae8a507
vasylovyevhenclg-tech <vasylov.yevhen.clg@chnu.edu.ua>
2025-10-03 15:22:27 +0300

    Update math_lib.h

--- src/math_lib/include/math_lib.h
+++ src/math_lib/include/math_lib.h
@@ -70,3 +70,12 @@ namespace MathLib
     // Function to search GCD
     int GCD(int a, int b);
 }
+
+int factorial(int n) {
+    if (n < 0) return -1; 
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}

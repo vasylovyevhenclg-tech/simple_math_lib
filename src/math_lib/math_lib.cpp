3899fe6481d7434309f97f5ceab5c1a57b65de58
vasylovyevhenclg-tech <vasylov.yevhen.clg@chnu.edu.ua>
2025-10-13 20:10:03 +0300

    Update math_lib.cpp

--- src/math_lib/math_lib.cpp
+++ src/math_lib/math_lib.cpp
@@ -45,10 +45,9 @@ namespace MathLib
 }
 
 int factorial(int n) {
-         if (n < 0) return -1; 
-         int result = 1;
-         for (int i = 1; i <= n; i++) {
-             result *= i;
-         }
-         return result;
-     }
+    if (n < 0) return -1; // помилка
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+        result *= i;
+    return result;
+}
